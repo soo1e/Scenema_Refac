188bca2f270f56c4a79c52d92107cadd6a1958c0
soo1e <gameswuon@gmail.com>
2024-11-25 21:43:50 +0900

    Initial commit

---
+++ src/main/java/com/soo1e/scenema/ScenemaApplication.java
@@ -0,0 +1,13 @@
+package com.soo1e.scenema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScenemaApplication.class, args);
+    }
+
+}
